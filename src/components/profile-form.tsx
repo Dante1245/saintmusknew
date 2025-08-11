fe3a09a65d540a357fb10b9dc678f4cd92fc19f7
Clinton Saintkid <clintonsaintkid9971@gmail.com>
2025-08-11 06:43:26 +0000

    Runtime Error
    Error: FormDescription is not defined
    
    src/components/pr

--- src/components/profile-form.tsx
+++ src/components/profile-form.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import { useForm } from "react-hook-form";
@@ -14,6 +15,7 @@ import {
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
