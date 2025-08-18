f613c25575c368bb307e0fa083579654ca3ffdab
Clinton Saintkid <clintonsaintkid9971@gmail.com>
2025-08-18 23:30:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,31 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
         remotePatterns: [
-            { protocol: 'https', hostname: 'images.unsplash.com' },
-            { protocol: 'https', hostname: 'api.qrserver.com' },
-            { protocol: 'https', hostname: 'coin-images.coingecko.com' },
-            { protocol: 'https', hostname: 'assets.coingecko.com' },
-            { protocol: 'https', hostname: 'images.cryptocompare.com' },
-            { protocol: 'https', hostname: 'randomuser.me' },
+            {
+                protocol: 'https',
+                hostname: 'resources.cryptocompare.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'api.qrserver.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'randomuser.me',
+                port: '',
+                pathname: '/**',
+            }
         ],
     },
 };
